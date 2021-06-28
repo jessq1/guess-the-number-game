5eb60ddab3b0a4c775ba1d0b043bc0725020e65b
JessQi <85958041+jessq1@users.noreply.github.com>
2021-06-27 23:46:39 -0400

    fix object functions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let secretNum, guessList, isWinner;
+let isWinner = false;
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -13,110 +13,158 @@ const guessBtn = document.getElementById('guessButton');
 const resetBtn = document.getElementById('resetButton');
 const guessInput = document.getElementById('guessInput');
 
+/*-------------------------------- Objects --------------------------------*/
+
+const game = {
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses: [],
+    guess: parseInt(guessInput.value),
+    play: function(guess) {
+      if (this.prevGuesses.length === 0) {
+        guessesEl.innerText = 'Previous Guesses:'
+      }
+      if (isWinner === false) {
+        game.getGuess(guess);
+      }
+      this.render(guess);
+     },
+    init: function(){
+      messageEl.className = '';
+      guessesEl.innerText = '';
+      messageEl.innerText = `Please enter a number between ${this.smallestNum} and ${this.biggestNum}.`
+      guessInput.value = '';
+      isWinner = false;
+      this.prevGuesses = [];
+      this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum 
+      this.render();
+      //answer check:
+      document.getElementById("answer").innerHTML = this.secretNum;
+    },
+    getGuess: function(guess) {
+      guessInput.value = '';
+      if (isNaN(guess)){
+        messageEl.innerText = 'Whoops! Please enter a number - try again';
+      } else if(guess < this.smallestNum || guess > this.biggestNum) {
+        messageEl.innerText = `Whoops! Please try a number between ${this.smallestNum} and ${this.biggestNum}.`;
+      } else if (guess === this.secretNum) {
+        messageEl.className = 'winner';
+        isWinner = true;
+        this.prevGuesses.push(guess)
+        if (this.prevGuesses.length === 0) {
+          messageEl.innerText = `WOW, LOOK AT THAT! You found the number in ${this.prevGuesses.length} guess!`
+        } else {
+          messageEl.innerText = `Congratulations! You found the number in ${this.prevGuesses.length} guesses!`
+        }
+      } else if (guess <= this.secretNum) {
+        messageEl.className = 'low';
+        messageEl.innerText = `${guess} is too low, please try again!`
+        this.prevGuesses.push(guess);
+      } else {
+        messageEl.className = 'high';
+        messageEl.innerText = `${guess} is too high, please try again!`
+        this.prevGuesses.push(guess);
+      }
+    },
+    render: function(guess){
+      if (guess > this.secretNum) {
+        let div = document.createElement("div");
+        div.innerText = guess;
+        div.className = "high";
+        guessesEl.appendChild(div);
+      } else if (guess < this.secretNum) {
+        let div = document.createElement("div");
+        div.innerText = guess;
+        div.className = "low";
+        guessesEl.appendChild(div);
+      } else if (guess === this.secretNum) {
+        let div = document.createElement("div");
+        div.innerText = guess;
+        div.className = "winner";
+        guessesEl.appendChild(div);
+      }
+    }
+  }
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 resetBtn.addEventListener('click', function() {
-  init();
+  game.init();
 });
 
+// guessBtn.addEventListener('click', game.play())
+
 guessBtn.addEventListener('click', function() {
-  if (guessList.length === 0) {
+  if (game.prevGuesses.length === 0) {
     guessesEl.innerText = 'Previous Guesses:'
   }
   if (isWinner === false) {
-    checkGuess(parseInt(guessInput.value));
+    game.play(parseInt(guessInput.value));
   }
 })
 
 
 /*-------------------------------- Functions --------------------------------*/
-init();
+game.init();
 
-function init() {
-  messageEl.className = '';
-  guessesEl.innerText = '';
-  messageEl.innerText = 'Please enter a number between 1 and 100'
-  guessInput.value = '';
-  guessList = [];
-  isWinner = false;
-  secretNum = Math.floor(Math.random()*5) + 1;
-  render();
-}
-function checkGuess (guess) {
-  guessInput.value = '';
-  if (isNaN(guess)){
-    messageEl.innerText = 'Whoops! Please enter a number - try again';
-  } else if(guess < 1 || guess > 100) {
-    messageEl.innerText = 'Whoops! Please try a number between 1 and 100.';
-  } else if (guess === secretNum) {
-    messageEl.className = 'winner';
-    isWinner = true;
-    guessList.push(guess)
-    if (guessList.length === 0) {
-      messageEl.innerText = `WOW, LOOK AT THAT! You found the number in ${guessList.length} guess!`
-    } else {
-      messageEl.innerText = `Congratulations! You found the number in ${guessList.length} guesses!`
-    }
-  } else if (guess <= secretNum) {
-    messageEl.className = 'low';
-    messageEl.innerText = `${guess} is too low, please try again!`
-    guessList.push(guess);
-  } else {
-    messageEl.className = 'high';
-    messageEl.innerText = `${guess} is too high, please try again!`
-    guessList.push(guess);
-  }
-  render(guess);
-}
-
-function render(guess) {
-  if (guess > secretNum) {
-    let div = document.createElement("div");
-    div.innerText = guess;
-    div.className = "high";
-    guessesEl.appendChild(div);
-  } else if (guess < secretNum) {
-    let div = document.createElement("div");
-    div.innerText = guess;
-    div.className = "low";
-    guessesEl.appendChild(div);
-  } else if (guess === secretNum) {
-    let div = document.createElement("div");
-    div.innerText = guess;
-    div.className = "winner";
-    guessesEl.appendChild(div);
-  }
-}
-
-// const game = {
-//     title: 'Guess the Number!',
-//     biggestNum: 10,
-//     smallestNum: 1,
-//     secretNum: null,
-//     play: function() {
-//       this.secretNum = Math.floor(Math.random() * 
-//         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-//         document.getElementById("answer").innerHTML = this.secretNum;
-//       //   do {
-//       //     this.prevGuesses.push((this.getGuess()))
-//       //     document.getElementById("guessInput").innerHTML = this.prevGuesses;
-//       // } while (this.getGuess() != this.secretNum);
-//      },
-//     prevGuesses: [],
-//     getGuess: function() {
-//       let currentGuess = '';
-//       while (currentGuess != 'quit'){
-//         currentGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} or "quit" to exit:`)
-//           if (currentGuess <= this.biggestNum && currentGuess >= this.smallestNum){
-//             return currentGuess
-//           } else if (!isNaN(parseInt(currentGuess))){
-//             alert(`Please enter a number within range`)
-//           } else {
-//             alert('Please enter a number - try again');
-//           }
-//       }
+// function init() {
+//   messageEl.className = '';
+//   guessesEl.innerText = '';
+//   messageEl.innerText = 'Please enter a number between 1 and 100'
+//   guessInput.value = '';
+//   guessList = [];
+//   isWinner = false;
+//   secretNum = Math.floor(Math.random()*5) + 1;
+//   render();
+// }
+// function checkGuess (guess) {
+//   guessInput.value = '';
+//   if (isNaN(guess)){
+//     messageEl.innerText = 'Whoops! Please enter a number - try again';
+//   } else if(guess < 1 || guess > 100) {
+//     messageEl.innerText = 'Whoops! Please try a number between 1 and 100.';
+//   } else if (guess === secretNum) {
+//     messageEl.className = 'winner';
+//     isWinner = true;
+//     guessList.push(guess)
+//     if (guessList.length === 0) {
+//       messageEl.innerText = `WOW, LOOK AT THAT! You found the number in ${guessList.length} guess!`
+//     } else {
+//       messageEl.innerText = `Congratulations! You found the number in ${guessList.length} guesses!`
 //     }
+//   } else if (guess <= secretNum) {
+//     messageEl.className = 'low';
+//     messageEl.innerText = `${guess} is too low, please try again!`
+//     guessList.push(guess);
+//   } else {
+//     messageEl.className = 'high';
+//     messageEl.innerText = `${guess} is too high, please try again!`
+//     guessList.push(guess);
 //   }
+//   render(guess);
+// }
+
+// function render(guess) {
+//   if (guess > secretNum) {
+//     let div = document.createElement("div");
+//     div.innerText = guess;
+//     div.className = "high";
+//     guessesEl.appendChild(div);
+//   } else if (guess < secretNum) {
+//     let div = document.createElement("div");
+//     div.innerText = guess;
+//     div.className = "low";
+//     guessesEl.appendChild(div);
+//   } else if (guess === secretNum) {
+//     let div = document.createElement("div");
+//     div.innerText = guess;
+//     div.className = "winner";
+//     guessesEl.appendChild(div);
+//   }
+// }
+
 
 // console.log(game.smallestNum)
 // game.getGuess()
