14648dc3cf8a9cf846e30a775ab1fa9d7c5acb65
JessQi <85958041+jessq1@users.noreply.github.com>
2021-06-27 20:17:22 -0400

    update getGuess method

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ const game = {
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    },
+    prevGuesses: [],
+    getGuess: function() {
+      let currentGuess = '';
+      while (currentGuess != 'quit'){
+        currentGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} or "quit" to exit:`)
+        if (currentGuess != 'quit'){
+          if (currentGuess <= this.biggestNum && currentGuess >= this.smallestNum){
+            this.prevGuesses.push(currentGuess)
+          } else if (!isNaN(parseInt(currentGuess))){
+            alert(`Please enter a number within range`)
+          } else if (currentGuess != 'quit'){
+            alert('Please enter a number - try again');
+          }
+        }
+      }
     }
   }
+
+  game.getGuess()
+// console.log(game.smallestNum)
+
